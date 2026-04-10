dd72be32ce5a0b9d1a0ea4f38527a7fe56eb4ea3
Rancy21 <rancyontsandaga@gmail.com>
2026-04-10 21:17:16 +0200

    add custom exception classes for token handling

---
+++ src/main/java/com/larr/auth/exception/InvalidTokenException.java
@@ -0,0 +1,8 @@
+package com.larr.auth.exception;
+
+public class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+
+}
