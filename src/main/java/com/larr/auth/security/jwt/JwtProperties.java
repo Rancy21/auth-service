c9ef2579b19d7b02c0fc01423a44d0c94457fb0b
Rancy21 <rancyontsandaga@gmail.com>
2026-04-17 15:26:53 +0200

    add validation starter and fix config properties

--- src/main/java/com/larr/auth/security/jwt/JwtProperties.java
+++ src/main/java/com/larr/auth/security/jwt/JwtProperties.java
@@ -1,17 +1,14 @@
 package com.larr.auth.security.jwt;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 
-@Component
+@Validated
 @ConfigurationProperties(prefix = "jwt")
-@Getter
-@Setter
-public class JwtProperties {
-    private String secret;
-    private long accessTokenExpiration;
-    private long refreshTokenExpiration;
+public record JwtProperties(@NotBlank String secret, Token accessToken, Token refreshToken) {
+    public record Token(@Positive long expiration) {
+    }
 }
